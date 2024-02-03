853cf12b60a991571f9516a7d4fd5177581893db
Carlos Monier <carlos.monier872@gmail.com>
2024-02-02 22:17:12 -0500

    2febrero

--- src/components/navbar/desktop-nav.jsx
+++ src/components/navbar/desktop-nav.jsx
@@ -1,19 +1,15 @@
 import { Link } from "react-scroll";
 
-
- // SCSS
-import './navbar.scss';
 // Assets
-//import LogoImg from '../../assets/navbar/logo.svg';
 import Logo from '../../assets/footer/LogoFooter.png';
 import MobileMenuIcon from '../../assets/navbar/mobile-menu.svg';
 
 const desktopNav = (props) => (
-  <nav className={`Navbar ${!props.userIsScrolled ? "extraLargeNavbar" : ""}`}>
+  <nav className={`Navbar ${!props.userIsScrolled ? "extraLargeNavbar" : ""}`} style={{ backgroundColor: '#f0f0f0' }}>
     <div className="wrapper flex-s-between">
-      <div>
+      <div style={{ marginLeft: '20px' }}>
         <Link to="hero" spy={true} smooth={true} offset={0} duration={200}>
-          <img src={Logo}  style={{ width: '130px' }} alt="logo" className="pointer" />
+          <img src={Logo} style={{ width: '130px' }} alt="logo" className="pointer" />
         </Link>
       </div>
       <div className="mobile__menu" onClick={props.mobileMenuOpen}>
@@ -23,24 +19,24 @@ const desktopNav = (props) => (
         <ul className="flex-s-between">
           <li>
             <Link activeClass="active-link" to="portfolio" spy={true} smooth={true} offset={-70} duration={500}>
-              SERVICIOS
+              <span style={{ fontSize: '20px' }}>SERVICIOS</span>
             </Link>
           </li>
           <li>
             <Link activeClass="active-link" to="about" spy={true} smooth={true} offset={-70} duration={500}>
-              SOBRE NOSOTROS
+              <span style={{ fontSize: '20px' }}>SOBRE NOSOTROS</span>
             </Link>
           </li>
           <li>
             <Link activeClass="active-link" to="contact" spy={true} smooth={true} offset={-70} duration={500}>
-              CONTACTOS
+              <span style={{ fontSize: '25px' }}>CONTACTOS</span>
             </Link>
           </li>
           <li>
-          <Link activeClass="active-link" to="contact" spy={true} smooth={true} offset={-70} duration={500}>
-            PROFORMA CONTRATO
-          </Link>
-        </li>
+            <Link activeClass="active-link" to="contact" spy={true} smooth={true} offset={-70} duration={500}>
+              <span style={{ fontSize: '25px' }}>PROFORMA CONTRATO</span>
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
